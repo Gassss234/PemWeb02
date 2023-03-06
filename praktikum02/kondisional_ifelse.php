bd6dd612f5dced3805bfdee28a1b8702f4d32140
Gassss234 <114986443+Gassss234@users.noreply.github.com>
2023-03-06 13:08:04 +0700

    Add files via upload

---
+++ praktikum02/kondisional_ifelse.php
@@ -0,0 +1,11 @@
+<?php
+$nasi = "ditumbuk";
+
+if($nasi == "digoreng"){
+    echo "Ya, Nasi Digoreng";
+} elseif($nasi == "dibakar"){
+    echo "Ya, Nasi Dibakar";
+} else {
+    echo "Ya, ini nasi Bubur";
+}
+
